12bf9cb1c2bc32d23ccdcd370033caa3b78db224
jenne <jenne@DESKTOP-GGMB2PC>
2026-02-20 12:33:47 +0300

    enumlar

---
+++ Konu11Enumlar/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Konu11Enumlar
+{
+    internal class Program
+    {
+        //Enum(numaratör) lar nesneleri numaralandırmak için kullanılır
+        /*
+        * enum tipler üzerindeki kısıtlar
+        *1-enum blokunda metot tanımlanamaz
+        *2-arayüz(interface )  kullanamazlar
+        *3-enum bloğunda property kullanılmaz
+          */
+        enum Aylar : byte
+        {
+            Ocak = 1, şubat, mart, nisan, mayıs, haziran, temmuz, ağustos, eylül, kasım, aralık
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Konu11Enumlar");
+        }
+    }
+}
