b3d4ef92311ad1164604da43be4255c862e87b8b
jenne <jenne@DESKTOP-GGMB2PC>
2026-03-02 14:11:32 +0300

    finish

--- Konu13KapsullemeEncapsulation/Program.cs
+++ Konu13KapsullemeEncapsulation/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Konu13KapsullemeEncapsulation
+﻿using System.Threading.Channels;
+
+namespace Konu13KapsullemeEncapsulation
 {
     internal class Bolum
     {
@@ -25,11 +27,38 @@
         {
             Console.WriteLine("Konu13 Kapsulleme Encapsulation");
             Console.WriteLine("metot ile kapsulleme");
-            Bolum bolum = new Bolum();
-            Console.WriteLine("hanği alanda eğitim almak istersiniz");
-            var bolumAdi = Console.ReadLine();
-            bolum.SetBolumAdi(bolumAdi);
-            Console.WriteLine("bölüm:"+ bolum.GetBolumAdi());
+            Bolum bolum = new Bolum();  //bölüm  classından bölüm adında bir nesne üret
+            Console.WriteLine("hanği alanda eğitim almak istersiniz");  
+            var bolumAdi = Console.ReadLine();   // ekrandan girilecek değeri oku 
+            bolum.SetBolumAdi(bolumAdi);   //girilen değeri bölüm nesnelerindeki set metoduna gönder 
+            Console.WriteLine("bölüm:"+ bolum.GetBolumAdi());  //bölüm nesnesindeki metotla private değişkenin değerini oku
+            Console.WriteLine();
+            // property ile kapsülleme
+            Console.WriteLine(  "property ile kapsülleme");
+            Fakulte fakulteNesnesi = new Fakulte();  //burada nesneyi oluşturuyoruz
+            fakulteNesnesi.Bolum = bolumAdi; // veri atama : set bloğunu çalıştırır.
+            Console.WriteLine("Fakülte bölüm:"+fakulteNesnesi.Bolum); // veri okuma : get bloğğunu çalıştırır
+        } 
+
+    }
+    public class Fakulte
+    {
+        private string bolum;
+        public string Bolum
+        {
+            get { return bolum; }
+            set
+            {
+                if (value=="yazılım eğitimi")  // eğer gönderilen değer yazılım eğitim ise 
+                {
+                    bolum = value;  // property e değer atamasına izin ver 
+                }
+                else
+                {
+                    Console.WriteLine("kurumumuzda bu eğitim verilmemektedir");
+                }
+                
+            }
         }
     }
 }
---
+++ Konu14InterfacesArayuzler/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace Konu14InterfacesArayuzler
+{
+    public interface OrnekArayuz   // class yerine interface kelimesini kullanarak tanımlıyoruz
+    {
+        public int Id { get; set;  }
+    }
+    interface Idemo
+    {
+        void Goster(); //metot imzası 
+    }
+    interface icerebilicekleri : Idemo // interface ler başka interfacelerden miras ala bilirler 
+    {
+        // bu inteface  i kullanacak olan classlar aşağıdaki varlıkları kullanmak zorundadır.
+        public int sayi { get; set; }
+        public static int sayi2 { get; set; }
+
+        // interface lerde metotlar değil sadece imzalar yer alır
+        void Topla();  // geri değer döndürmeyen metot imzası 
+        int ToplamaYap();  // geri int değer döndürmesi gereken metot imzası 
+    }
+    class ArayuzKullanımı : icerebilicekleri  // bu şekilde miras aldığımızda interface ler içerisindeki varlıklar class ta kullanılmak zorundadır 
+    {
+        public int sayi { get; set; }
+        public int Id { get; set; }  // class içerisinde interface de olmayan ama class ta olması gereken property ve ya metotlar buluna bilir 
+
+       
+
+        public void Goster()  // interface de imzası bulunan metot class içinde kullanılmak zorundadır 
+        {
+            Console.WriteLine("void  Goster metodu");
+        }
+
+        public void Topla()
+        {
+            Console.WriteLine("void topla metodu");
+        }
+
+        public int ToplamaYap()
+        {
+            return sayi + 1;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Konu14InterfacesArayuzler");
+            ArayuzKullanımı arayuz = new ArayuzKullanımı();
+            arayuz.sayi = 1;
+            arayuz.Topla();
+            arayuz.Goster();
+            Console.WriteLine("toplama sonucu : "+ arayuz.ToplamaYap());
+        }
+    }
+}
